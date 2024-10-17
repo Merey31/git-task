e72ac1b8319450ffa538611c8cacee68e87b974a
Baimagambetov Nurbek <nurbek.ekb2z@gmail.com>
2024-10-17 19:47:07 +0500

    Добавлен первый куплет

---
+++ src/lyrics/txt.java
@@ -0,0 +1,12 @@
+package lyrics;
+
+public class txt {
+    void lyrics(String[] args) {
+        System.out.println("""
+                Here comes the sun, doo-doo-doo-doo
+                Here comes the sun, and I say
+                It's alright
+                Little darlin', it's been a long, cold, lonely winter
+                Little darlin', it feels like years since it's been here""");
+    }
+}
